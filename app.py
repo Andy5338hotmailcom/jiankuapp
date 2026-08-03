b29cf145b3223607c455739c7524380f69eea3d7
Andy5338hotmailcom <Andy_5338@hotmail.com>
2026-08-04 06:44:13 +0800

    Implement persistent storage for webview application
    Added functionality to create a persistent storage directory for the application.

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import webview
 import os
 
-# 核心：加载同目录下的index.html（外部文件）
 if __name__ == '__main__':
-    # 获取index.html的绝对路径（确保能找到同目录的外部HTML）
     html_path = os.path.join(os.path.dirname(__file__), 'start.html')
-    
-    # 创建窗口并加载外部HTML文件
+    # 持久化目录
+    appdata = os.getenv("APPDATA")
+    storage_dir = os.path.join(appdata, r"wg\jiankuapp\cookie")
+    os.makedirs(storage_dir, exist_ok=True)
+
+    # 创建窗口
     webview.create_window(
         title='简库资源网',
-        url=html_path,  # 关键：加载外部HTML文件，而非字符串
+        url=html_path,
         width=800,
         height=600
     )
-    webview.start()
+
+    # 关键：private_mode=False 关闭隐私模式，启用持久存储
+    webview.start(
+        storage_path=storage_dir,
+        private_mode=False
+    )
